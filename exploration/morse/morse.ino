4f3120f50b2f7a0bdfd9940477dd1dc46608f17f
jeffrey k eliasen <jeff@jke.net>
2022-09-18 14:04:53 -1000

    complete morse code table and improve representations

--- exploration/morse/morse.ino
+++ exploration/morse/morse.ino
@@ -15,7 +15,7 @@ const int DELAY_DOT = DELAY_SHORT;
 const int DELAY_DASH = DELAY_DOT * 2.5;
 const int DELAY_CHARACTER_BREAK = DELAY_DASH * 2;
 const int DELAY_WORD_BREAK = DELAY_DASH * 3;
-const int DELAY_UNKNOWN_CHARACTER = DELAY_DASH * 4;
+const int DELAY_UNKNOWN_CHARACTER = DELAY_DASH * 5;
 
 // SETUP AND MAIN LOOP
 
@@ -24,40 +24,88 @@ void setup() {
 }
 
 void loop() {
-  emit('s');
-  emit('o');
-  emit('s');
-
-  wordBreak();
+  emit("the quick brown fox jumps over the lazy dog");
 }
 
 // HELPERS
 
-void emit(char ch) {
-  switch (ch) {
-    case 'a':
-      dot();
-      dash();
-      break;
-    case 'b':
-      dash();
-      dot();
-      dot();
-      dot();
-      break;
-    case 'o':
-      dash();
-      dash();
-      dash();
-      break;
-    case 's':
-      dot();
-      dot();
-      dot();
-      break;
-    default:
-      blink(DELAY_UNKNOWN_CHARACTER);
-      break;
+void emit(char* s) {
+  while (*s) {
+    emitChar(*s);
+    *s++;
+  }
+
+  wordBreak();
+}
+
+void emitChar(char c) {
+  char* s = getSequence(c);
+  emitSequence(s);
+}
+
+char* getSequence(char c) {
+  switch (c) {
+    case 'a': return ".-";
+    case 'b': return "-...";
+    case 'c': return "-.-.";
+    case 'd': return "-..";
+    case 'e': return ".";
+    case 'f': return "..-.";
+    case 'g': return "--.";
+    case 'h': return "....";
+    case 'i': return "..";
+    case 'j': return ".---";
+    case 'k': return "-.-";
+    case 'l': return ".-..";
+    case 'm': return "--";
+    case 'n': return "-.";
+    case 'o': return "---";
+    case 'p': return ".--.";
+    case 'q': return "--.-";
+    case 'r': return ".-.";
+    case 's': return "...";
+    case 't': return "-";
+    case 'u': return "..-";
+    case 'v': return "...-";
+    case 'w': return ".--";
+    case 'x': return "-..-";
+    case 'y': return "-.--";
+    case 'z': return "--..";
+    case '0': return "-----";
+    case '1': return ".----";
+    case '2': return "..---";
+    case '3': return "...--";
+    case '4': return "....-";
+    case '5': return ".....";
+    case '6': return "-....";
+    case '7': return "--...";
+    case '8': return "---..";
+    case '9': return "----.";
+    case ' ': return " ";
+    default: return "!";
+  }
+}
+
+void emitSequence(char* s) {
+  if (!s) {
+    blink(DELAY_UNKNOWN_CHARACTER);
+  }
+  while (*s) {
+    switch (*s) {
+      case '.':
+        dot();
+        break;
+      case '-':
+        dash();
+        break;
+      case ' ':
+        wordBreak();
+        break;
+      default:
+        blink(DELAY_UNKNOWN_CHARACTER);
+        break;
+    }
+    *s++;
   }
   characterBreak();
 }
